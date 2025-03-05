3429c1c527bf9261c363034b7ef747bf7d6368b4
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2025-03-05 20:24:10 +0100

    updating file local

--- components/cards/CastCrewCard.tsx
+++ components/cards/CastCrewCard.tsx
@@ -2,11 +2,15 @@ import Image from 'next/image';
 import React from 'react';
 
 const CastCrewCard = ({ name, profilePath, role }) => {
+  const imageSrc = profilePath
+    ? `https://image.tmdb.org/t/p/w200${profilePath}`
+    : '/nopic.jpg';
+
   return (
     <div className="relative items-center justify-center w-full h-full rounded-lg overflow-hidden bg-dark-gray-blue border-2 border-pink-500 group">
       <div className="relative w-full h-32">
         <Image
-          src={`https://image.tmdb.org/t/p/w200${profilePath}`}
+          src={imageSrc}
           alt={name}
           width={200}
           height={300}
