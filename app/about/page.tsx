2c7232283660f4e6b7ba64d5dc423a30c3ffa792
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2025-03-15 23:13:49 +0100

    Enhanced the About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,74 +1,66 @@
 'use client';
 import React from 'react';
-import { FESTIVAL_NAMES } from '../../utils/constants';
+import SmallCardInfo from '../../components/about/SmallFilmCardInfo';
+import FilmPageInfo from '../../components/about/FilmPageInfo';
+import ContactSection from '../../components/about/ContactInfo';
+import Link from 'next/link';
+import OramaInfo from '../../components/about/OramaInfo';
+import WhoWeAreInfo from '../../components/about/WhoWeAreInfo';
 
 const About = () => {
   return (
     <div className="w-full min-h-screen bg-gradient-dark-gray-blue text-white flex flex-col items-center py-8">
-      <header className="text-center mb-12">
+      <header className="text-center">
         <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 via-purple-500 to-blue-500 mb-4">
-          Welcome to the indie film haven
+          About this cool place on the internet
         </h1>
-        <p className="text-lg mt-2">
-          where the popcorn is stale, the aspect ratios are weird, and the
-          budget is mostly vibes.
-        </p>
       </header>
-      <section className="w-full max-w-3xl mx-auto px-6 py-8 bg-gradient-dark-gray-blue rounded-xl shadow-2xl space-y-6">
-        <h2 className="text-2xl font-semibold text-pink-500">
-          Who We Are - What We Do
-        </h2>
-        <p className="text-lg mt-2">
-          We’re indie film lovers, festival junkies, and champions of movies
-          made on dreams and maxed-out credit cards. We dig through festivals to
-          find hidden gems, bizarre masterpieces, and the occasional two-hour
-          close-up of a potato. If you love cinema that breaks rules and
-          budgets, welcome to the tribe.
-        </p>
-        <div>
-          <h2 className="text-2xl font-semibold text-pink-500">
-            Film Festivals
-          </h2>
-          <p className="text-lg mt-2">
-            We’ve databased indie films from these prestigious festivals:
-          </p>
-          <ul className="list-disc list-inside mt-2 text-lg">
-            {Object.keys(FESTIVAL_NAMES).map((festival) => (
-              <li key={festival} className="mt-1">
-                {FESTIVAL_NAMES[festival]}
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div>
-          <h2 className="text-2xl font-semibold text-pink-500">
-            Help Us Improve
-          </h2>
-          <p className="text-lg mt-2">
-            We’re always working to improve. If you spot any bugs, errors, or
-            issues with the films or anything else, feel free to let us know.
-            Your help makes this platform better!
-          </p>
-        </div>
+      <section className="w-full max-w-3xl mx-auto px-6 py-8 bg-gradient-dark-gray-blue rounded-xl shadow-2xl space-y-6 mt-8">
+        <ContactSection />
       </section>
-
-      {/* Call to Action Section */}
-      <section className="mt-12 text-center">
-        <h3 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-400 via-purple-500 to-blue-500 mb-4">
-          Ready to explore the indie world?
-        </h3>
-        <p className="text-lg mb-6">
-          Let’s get started on your indie film journey—no fluff, just real
-          cinema.
-        </p>
-        <a
-          href="/film-search"
-          className="px-6 py-3 bg-pink-500 text-white rounded-lg shadow-lg hover:bg-pink-600 transition"
-        >
-          Go to Films
-        </a>
+      <section className="w-full max-w-3xl mx-auto px-6 py-8 bg-gradient-dark-gray-blue rounded-xl shadow-2xl space-y-6  mt-8">
+        <WhoWeAreInfo />
       </section>
+      <section className="w-full max-w-3xl mx-auto px-6 py-8 bg-gradient-dark-gray-blue rounded-xl shadow-2xl space-y-6 mt-8">
+        <OramaInfo />
+      </section>
+      <section className="w-full max-w-3xl mx-auto px-6 py-8 bg-gradient-dark-gray-blue rounded-xl shadow-2xl space-y-6 mt-8">
+        <FilmPageInfo />
+      </section>
+      <section className="w-full max-w-3xl mx-auto px-6 py-8 bg-gradient-dark-gray-blue rounded-xl shadow-2xl space-y-6 mt-8">
+        <SmallCardInfo />
+      </section>
+      <div className="relative z-10 text-center m-4 flex flex-col border-t-2 border-t-pink-500">
+        <h1 className="text-sm text-transparent font-extrabold bg-clip-text bg-gradient-to-r from-pink-400 via-purple-500 to-blue-500 text-left mt-6 ml-2">
+          This is an independent, open-source web app developed by{' '}
+          <Link
+            href="https://matteo.codes"
+            className="text-transparent bg-clip-text bg-gradient-to-r from-pink-400 via-purple-500 to-blue-500"
+          >
+            matteo.codes
+          </Link>
+          . Content is for informational purposes only. The developers do not
+          guarantee accuracy or reliability. Use at your own risk and in
+          compliance with applicable laws. <br></br>This project is intended to
+          be open-source, and any help is truly appreciated. The repository can
+          be found at this{' '}
+          <Link
+            href="https://github.com/matteobu/whichfilm"
+            className="text-transparent bg-clip-text bg-gradient-to-r from-pink-400 via-purple-500 to-blue-500"
+          >
+            GitHub Repo
+          </Link>
+          . For inquiries, suggestions, or corrections, you can contact the
+          developers at{' '}
+          <Link
+            href="mailto:your-email@example.com"
+            className="text-transparent bg-clip-text bg-gradient-to-r from-pink-400 via-purple-500 to-blue-500"
+          >
+            matteo.codes@pm.me
+          </Link>
+          .
+        </h1>
+      </div>
     </div>
   );
 };
---
+++ components/about/ContactInfo.tsx
@@ -0,0 +1,36 @@
+import { FaGithub, FaDiscord, FaEnvelope } from 'react-icons/fa';
+
+function ContactSection() {
+  return (
+    <>
+      <div className="flex justify-evenly items-center space-x-6">
+        <a
+          href="mailto:matteo.codes@pm.me"
+          className="flex items-center space-x-2 text-white text-lg hover:text-pink-400"
+        >
+          <FaEnvelope size={30} />
+        </a>
+
+        <a
+          href="https://github.com/matteobu/whichfilm"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center space-x-2 text-white text-lg hover:text-pink-400"
+        >
+          <FaGithub size={30} />
+        </a>
+
+        <a
+          href="https://discord.gg/BEHSKHP8"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center space-x-2 text-white text-lg hover:text-pink-400"
+        >
+          <FaDiscord size={30} />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default ContactSection;
