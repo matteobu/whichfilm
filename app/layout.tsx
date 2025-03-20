ef3f210a0ae43288c449faf4fe4d5368d79d4b7e
Matteo Bucciol <62759388+matteobu@users.noreply.github.com>
2025-03-20 11:27:21 +0100

    Change Logo - database update - metatags added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,50 @@
 import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 import MainHeader from '../components/headers/MainHeader';
-import Footer from '../components/Footer';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="index, follow" />
+        <meta
+          name="description"
+          content="Find and explore festival gems with the best independent films. Search, discover, and filter films from top global film festivals."
+        />
+        <meta
+          name="keywords"
+          content="Film Festival, Independent Films, Film Database, Film Search, Festival Films, Indie Films"
+        />
+        <meta name="author" content="WhichFilm App" />
+        <meta
+          property="og:title"
+          content="WhichFilm - Discover Festival Gems"
+        />
+        <meta
+          property="og:description"
+          content="Search, discover, and filter through a curated database of festival films."
+        />
+        <meta property="og:image" content="/path-to-image.jpg" />{' '}
+        <meta property="og:url" content="https://whichfilm-xi.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta
+          name="twitter:title"
+          content="WhichFilm - Discover Festival Gems"
+        />
+        <meta
+          name="twitter:description"
+          content="Find, search, and explore independent films from top festivals with WhichFilm."
+        />
+        <meta name="twitter:image" content="/path-to-image.jpg" />{' '}
+        <meta name="twitter:card" content="summary_large_image" />
+        <title>WhichFilm - Discover Festival Gems</title>
+      </head>
       <body>
         <MainHeader />
         {children}
         <Analytics />
-
       </body>
     </html>
   );
--- components/headers/MainHeader.tsx
+++ components/headers/MainHeader.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import NavLink from './NavLink';
 import { PiFilmReel } from 'react-icons/pi';
@@ -9,14 +10,19 @@ export default function MainHeader() {
     <header className="bg-gradient-dark-gray-blue text-white p-3 sticky top-0 z-50 border-b-1 border-pink-500">
       <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
         <div className="flex space-x-2 items-center">
-          <Link
-            href="/"
-            className="text-pink-500 font-bold text-xl sm:text-2xl"
-          >
-            which film ?
+          <Link href="/" className="flex items-center space-x-2">
+            <Image
+              src="/logo.png"  
+              alt="whichfilm Logo"
+              width={20}  
+              height={20}  
+              className="sm:w-10 sm:h-10"  
+            />
+            <span className="text-pink-500 font-bold text-xl sm:text-2xl">
+              which film ?
+            </span>
           </Link>
         </div>
-
         <nav>
           <ul className="flex space-x-6 items-center h-full">
             <NavLink href="/film-search">
